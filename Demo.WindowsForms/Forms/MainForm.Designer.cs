57646742382a2c003b98b0fc38b7a22e51eb7e09
lsass <pritchinpavel@yandex.ru>
2012-01-31 21:14:31 +0400

    version 2.0 init

--- Demo.WindowsForms/Forms/MainForm.Designer.cs
+++ Demo.WindowsForms/Forms/MainForm.Designer.cs
@@ -171,6 +171,7 @@
           // 
           this.panelMenu.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
                       | System.Windows.Forms.AnchorStyles.Right)));
+          this.panelMenu.AssociatedSplitter = null;
           this.panelMenu.BackColor = System.Drawing.Color.Transparent;
           this.panelMenu.CaptionFont = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
           this.panelMenu.CaptionHeight = 27;
--- matsps.Wpf/MainWindow.xaml.cs
+++ matsps.Wpf/MainWindow.xaml.cs
@@ -15,6 +15,13 @@ using Microsoft.Windows.Controls.Ribbon;
 using GMap.NET.MapProviders;
 using GMap.NET;
 using GMap.NET.WindowsPresentation;
+using TreeViewwithCheckBoxes;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.IO;
+using Demo.WindowsPresentation.CustomMarkers;
+using matsps.Wpf.CustomMarkers;
+using System.Windows.Threading;
 
 namespace matsps.Wpf
 {
@@ -23,6 +30,16 @@ namespace matsps.Wpf
     /// </summary>
     public partial class MainWindow : RibbonWindow
     {
+        PointLatLng start;
+        PointLatLng end;
+
+       //internal readonly GMapOverlay routes = new GMapOverlay("routes");
+
+        public ObservableCollection<Node> Nodes { get; private set; }
+
+        // marker
+        GMapMarker currentMarker;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -42,11 +59,10 @@ namespace matsps.Wpf
             MainMap.MapProvider = GMapProviders.YandexMap;
             MainMap.Position = new PointLatLng(55.746474, 37.667109); // 37.667109    55.746474
             MainMap.MinZoom = 1;
-            MainMap.MaxZoom = 24;
+            MainMap.MaxZoom = 13;
             MainMap.Zoom = 10;
 
             // map events
-            /*
             MainMap.OnPositionChanged += new PositionChanged(MainMap_OnCurrentPositionChanged);
             MainMap.OnTileLoadComplete += new TileLoadComplete(MainMap_OnTileLoadComplete);
             MainMap.OnTileLoadStart += new TileLoadStart(MainMap_OnTileLoadStart);
@@ -55,10 +71,8 @@ namespace matsps.Wpf
             MainMap.MouseLeftButtonDown += new System.Windows.Input.MouseButtonEventHandler(MainMap_MouseLeftButtonDown);
             MainMap.Loaded += new RoutedEventHandler(MainMap_Loaded);
             MainMap.MouseEnter += new MouseEventHandler(MainMap_MouseEnter);
-             */
  
-            // get map types
-            /*
+            // get map types            
             comboBoxMapType.ItemsSource = GMapProviders.List;
             comboBoxMapType.DisplayMemberPath = "Name";
             comboBoxMapType.SelectedItem = MainMap.MapProvider;
@@ -67,6 +81,9 @@ namespace matsps.Wpf
             comboBoxMode.ItemsSource = Enum.GetValues(typeof(AccessMode));
             comboBoxMode.SelectedItem = MainMap.Manager.Mode;
 
+
+
+            /*
             // get cache modes
             checkBoxCacheRoute.IsChecked = MainMap.Manager.UseRouteCache;
             checkBoxGeoCache.IsChecked = MainMap.Manager.UseGeocoderCache;
@@ -90,6 +107,7 @@ namespace matsps.Wpf
 #endif
 
             //validator.Window = this;
+            */
 
             // set current marker
             currentMarker = new GMapMarker(MainMap.Position);
@@ -98,8 +116,7 @@ namespace matsps.Wpf
                 currentMarker.Offset = new System.Windows.Point(-15, -15);
                 currentMarker.ZIndex = int.MaxValue;
                 MainMap.Markers.Add(currentMarker);
-            }
-             */ 
+            } 
 
             /*
             //if(false)
@@ -161,7 +178,283 @@ namespace matsps.Wpf
             transport.ProgressChanged += new ProgressChangedEventHandler(transport_ProgressChanged);
             transport.WorkerSupportsCancellation = true;
             transport.WorkerReportsProgress = true;
-             */ 
+             */
+
+            //=====================================================
+            //      TrreView
+            //=====================================================
+            Nodes = new ObservableCollection<Node>();
+            FillingTree();
+        }
+
+        // tile loading stops
+        void MainMap_OnTileLoadComplete(long ElapsedMilliseconds)
+        {
+            MainMap.ElapsedMilliseconds = ElapsedMilliseconds;
+
+            System.Windows.Forms.MethodInvoker m = delegate()
+            {
+                progressBar1.Visibility = Visibility.Hidden;
+                //groupBox3.Header = "loading, last in " + MainMap.ElapsedMilliseconds + "ms";
+            };
+
+            try
+            {
+                this.Dispatcher.BeginInvoke(DispatcherPriority.Loaded, m);
+            }
+            catch
+            {
+            }
+        }
+
+        // tile louading starts
+        void MainMap_OnTileLoadStart()
+        {
+            System.Windows.Forms.MethodInvoker m = delegate()
+            {
+                progressBar1.Visibility = Visibility.Visible;
+            };
+
+            try
+            {
+                this.Dispatcher.BeginInvoke(DispatcherPriority.Loaded, m);
+            }
+            catch
+            {
+            }
+        }
+
+        // chenged map type
+        void MainMap_OnMapTypeChanged(GMapProvider type)
+        {
+            // !!! ZOOM SLIDER
+            //sliderZoom.Minimum = MainMap.MinZoom;
+            //sliderZoom.Maximum = MainMap.MaxZoom;
+        }
+
+        // move current marker with left holding
+        void MainMap_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (e.LeftButton == System.Windows.Input.MouseButtonState.Pressed)
+            {
+                System.Windows.Point p = e.GetPosition(MainMap);
+                currentMarker.Position = MainMap.FromLocalToLatLng((int)p.X, (int)p.Y);
+            }
+        }
+
+        void MainMap_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            System.Windows.Point p = e.GetPosition(MainMap);
+            currentMarker.Position = MainMap.FromLocalToLatLng((int)p.X, (int)p.Y);
+        }
+
+        // center markers on load
+        void MainMap_Loaded(object sender, RoutedEventArgs e)
+        {
+            MainMap.ZoomAndCenterMarkers(null);
+        }
+
+        void MainMap_MouseEnter(object sender, MouseEventArgs e)
+        {
+            MainMap.Focus();
+        }
+
+        /// <summary>
+        /// Take Id from CheckBox Uid and transfer value to CheckBoxId struct
+        /// </summary>
+        /// <param name="sender">The CheckBox clicked.</param>
+        /// <param name="e">Parameters associated to the mouse event.</param>
+        private void OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            CheckBox currentCheckBox = (CheckBox)sender;
+            CheckBoxId.checkBoxId = currentCheckBox.Uid;
+        }
+
+        /// <summary>
+        /// Filling tree different values
+        /// </summary>
+        private void FillingTree()
+        {
+            Nodes.Clear();
+            for (int i = 0; i < 5; i++)
+            {
+                var level_1_items = new Node() { Text = " Level 1 Item " + (i + 1) };
+                for (int j = 0; j < 2; j++)
+                {
+                    var level_2_items = new Node() { Text = " Level 2 Item " + (j + 1) };
+                    level_2_items.Parent.Add(level_1_items);
+                    level_1_items.Children.Add(level_2_items);
+                    for (int n = 0; n < 2; n++)
+                    {
+                        var level_3_items = new Node() { Text = " Level 3 Item " + (n + 1) };
+                        level_3_items.Parent.Add(level_2_items);
+                        level_2_items.Children.Add(level_3_items);
+                    }
+                }
+
+                Nodes.Add(level_1_items);
+            }
+            treeView.ItemsSource = Nodes;
+        }
+
+
+
+        // current location changed
+        void MainMap_OnCurrentPositionChanged(PointLatLng point)
+        {
+            mapgroup.Header = "gmap: " + point;
+        }
+
+        // reload
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            MainMap.ReloadMap();
+        }
+
+        // enable current marker
+        private void checkBoxCurrentMarker_Checked(object sender, RoutedEventArgs e)
+        {
+            if (currentMarker != null)
+            {
+                MainMap.Markers.Add(currentMarker);
+            }
+        }
+
+        // disable current marker
+        private void checkBoxCurrentMarker_Unchecked(object sender, RoutedEventArgs e)
+        {
+            if (currentMarker != null)
+            {
+                MainMap.Markers.Remove(currentMarker);
+            }
+        }
+
+        // enable map dragging
+        private void checkBoxDragMap_Checked(object sender, RoutedEventArgs e)
+        {
+            if(MainMap != null)
+                MainMap.CanDragMap = true;
+        }
+
+        // disable map dragging
+        private void checkBoxDragMap_Unchecked(object sender, RoutedEventArgs e)
+        {
+            MainMap.CanDragMap = false;
+        }
+
+        // change server\cache etc
+        private void comboBoxMode_DropDownClosed(object sender, EventArgs e)
+        {
+            MainMap.Manager.Mode = (AccessMode)comboBoxMode.SelectedItem;
+            MainMap.ReloadMap();
+        }
+
+        // change map
+        private void comboBoxMapType_DropDownClosed(object sender, EventArgs e)
+        {
+            MainMap.MapProvider = comboBoxMapType.SelectedItem as GMapProvider;
+            MainMap.ReloadMap();
+        }
+
+        private void button7_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                ImageSource img = MainMap.ToImageSource();
+                PngBitmapEncoder en = new PngBitmapEncoder();
+                en.Frames.Add(BitmapFrame.Create(img as BitmapSource));
+
+                Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+                dlg.FileName = "GMap.NET Image"; // Default file name
+                dlg.DefaultExt = ".png"; // Default file extension
+                dlg.Filter = "Image (.png)|*.png"; // Filter files by extension
+                dlg.AddExtension = true;
+                dlg.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+
+                // Show save file dialog box
+                bool? result = dlg.ShowDialog();
+
+                // Process save file dialog box results
+                if (result == true)
+                {
+                    // Save document
+                    string filename = dlg.FileName;
+
+                    using (System.IO.Stream st = System.IO.File.OpenWrite(filename))
+                    {
+                        en.Save(st);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        // enables tile grid view
+        private void checkBox1_Checked(object sender, RoutedEventArgs e)
+        {
+            MainMap.ShowTileGridLines = true;
+        }
+
+        // disables tile grid view
+        private void checkBox1_Unchecked(object sender, RoutedEventArgs e)
+        {
+            MainMap.ShowTileGridLines = false;
+        }
+
+        // sets route start
+        private void rbtnStart_Click(object sender, RoutedEventArgs e)
+        {
+            start = currentMarker.Position;
+        }
+
+        // sets route end
+        private void rbtnEnd_Click(object sender, RoutedEventArgs e)
+        {
+            end = currentMarker.Position;
+        }
+
+        // adds route
+        private void rbtnAddRoute_Click(object sender, RoutedEventArgs e)
+        {
+            RoutingProvider rp = MainMap.MapProvider as RoutingProvider;
+            if (rp == null)
+            {
+                rp = GMapProviders.GoogleMap; // use google if provider does not implement routing
+            }
+
+            MapRoute route = rp.GetRoute(start, end, false, false, (int)MainMap.Zoom);
+            if (route != null)
+            {
+                GMapMarker m1 = new GMapMarker(start);
+                m1.Shape = new CustomMarkerDemo(this, m1, "Start: " + route.Name);
+
+                GMapMarker m2 = new GMapMarker(end);
+                m2.Shape = new CustomMarkerDemo(this, m2, "End: " + start.ToString());
+
+                GMapMarker mRoute = new GMapMarker(start);
+                {
+                    mRoute.Route.AddRange(route.Points);
+                    mRoute.RegenerateRouteShape(MainMap);
+
+                    mRoute.ZIndex = -1;
+                }
+                rlRouteLength.Content = String.Format("{0:#.00} km", route.Distance );
+
+                MainMap.Markers.Add(m1);
+                MainMap.Markers.Add(m2);
+                MainMap.Markers.Add(mRoute);
+
+                MainMap.ZoomAndCenterMarkers(null);
+            }
+        }
+
+        private void rbtnClearRoute_Click(object sender, RoutedEventArgs e)
+        {
+            
         }
     }
+
 }
